0964b12f487476bc3818776c9d08c419f86afe81
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2023-03-01 19:05:25 +0200

    add old files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+
+import '../../src/index.css';
+import { ContactList } from "./ContactList/ContactList";
+import ContactForm from "./ContactForm/ContactForm";
+import Filter from "./Filter/Filter";
+
+// !!!!!!!!!!!!!! щоб додати глобально доступ до стор
+import { Provider } from "react-redux";
+import {store, persistor} from "../redux/store";
+
+import { PersistGate } from 'redux-persist/integration/react';
+
+const App = () => {
+
+  return <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <div className="container">
+        <div className="in_container">
+          <div>
+            <h2 className="title">Phonebook</h2>
+            <Filter/>
+            <ContactForm/>
+          </div>
+          <div>
+            <h2 className="title">Contacts</h2>
+            <ContactList/>
+          </div>
+        </div>
+        <div className="circle"></div>
+      </div>
+    </PersistGate>
+  </Provider>
+}
+
+export default App;
+
